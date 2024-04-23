bf57b39d84227fbd9031edd0563e0713ff46821f
Sarc4sticNerd <jplam904@gmail.com>
2024-04-22 18:43:50 -0700

    Home Page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect} from 'react';
 import gsap from 'gsap';
 import Typewriter from 'typewriter-effect';
 import { FaGithub,FaLinkedin } from "react-icons/fa";
@@ -14,6 +14,12 @@ const Home = () => {
     gsap.to(".b1", { duration: 2, width:"20rem",height:"20rem",repeat:-1,yoyo:true,ease: "power1.in", });
     gsap.to(".b2", { duration: 2,delay:1, width:"25rem",height:"25rem",repeat:-1,yoyo:true,ease: "power1.in", });
     gsap.to(".b3", { duration: 2,delay:-1, width:"15rem",height:"15rem",repeat:-1,yoyo:true,ease: "power1.in", });
+    gsap.to(".bar1", { duration: 2, width:"calc(61vw * .9)",ease: "power1.in", });
+    gsap.to(".bar2", { duration: 2, width:"calc(61vw * .8)",ease: "power1.in", });
+    gsap.to(".bar3", { duration: 2, width:"calc(61vw * .7)",ease: "power1.in", });
+
+
+    
 
   },[])
   return (
@@ -47,8 +53,8 @@ const Home = () => {
           <div className='navBox'>
             <a href="/"><FaHouse/>Home</a>
             <a href="/projects"><FaImage/>Projects</a>
-            <a href=""><FaSchool/>Training</a>
-            <a href=""><FaFile />Resume</a>
+            <a href="/training"><FaSchool/>Training</a>
+            <a href="/resume" rel="noreferrer" target="_blank"><FaFile />Resume</a>
           </div>
           <div className='bioBox'>Bio:
           <div>Jonathan Lam is a high school student at the West-MEC coding program aiming to become a proficient software developer and innovate in the tech industry.With a drive for learning and a knack for problem-solving, he's poised to make waves in the world of software development.</div>
@@ -59,9 +65,27 @@ const Home = () => {
           <div>jlam303@west-mec.org</div>
           <div>jlam303@west-mec.edu</div>
 
-          <div>111-111-1111</div>
+          <div>Phone: 111-111-1111</div>
           </div>
-          <div className='skillBox'>Skills:</div>
+          <div className='skillBox'>Skills:
+          <div className='skillBox2'>
+          <div className='skillBar'>
+            <div>HTML 90%:</div>
+            <div className='barOverlap bar1'></div>
+            <div className='bar'></div>
+          </div>
+          <div className='skillBar'>
+            <div>CSS 80%:</div>
+            <div className='barOverlap bar2'></div>
+            <div className='bar'></div>
+          </div>
+          <div className='skillBar'>
+            <div>JS 70%:</div>
+            <div className='barOverlap bar3'></div>
+            <div className='bar'></div>
+          </div>
+          </div>
+          </div>
         </div>
     </div>
   )
